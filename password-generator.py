85779e5e46280b974c76576095cada37da7c20a9
Ajeeveraj <ayushj762@deltalearns.ca>
2026-02-03 18:25:38 -0800

    finished the first question
    you can now give it a word but it will not be able do anything with the word so far.

--- password-generator.py
+++ password-generator.py
@@ -1,11 +1,29 @@
 import random
 import string
-
+    #Yes or no responses
 def yes_no():
-    print("Do you want a specific word to be in your passowrd?")
+    print("Do you want a specific word to be in your password?")
     
     user_input = input("(yes/no)")
     
     if user_input == "yes":
+        print("Okay, we'll add that word to your password.")
         word = input("Please enter your word. ")
+    elif user_input == "no":
+        print("Okay, your password will be entirely random.")
+    else:
+        print("Thats not a valid answer.")
+        return
+    # Asking the second question
+    upper_case = input("Would you like uppercase letters in your password? (yes/no)")
+    
+    if upper_case == "yes":
+        print("Your password will now have uppercased and lowercassed letters.")
+    elif upper_case == "no":
+        print("Okay, your password will be made up of only lowercase letters.")
+    else:
+        print("Type yes or no.")
+        return
+    
+    
      
